c4d07584b60d21d47387af0831a685ed98f1c0bd
lelandshir <lelandshir@gmail.com>
2021-03-11 00:22:00 -0500

    pushing notes

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
-# forms
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
 # Vue-3-Forms
+
+### Two-Way Data Binding and the v-model directive
+
+- The easiest way to track what a user types into an input field is v-model, another vue directive
+- it allows us to sync a form field with component data via two way data binding
+- component data and template inputs are bound
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>Vue Project 1.3: Forms</h1>
+	<SignUpForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUpForm from "./components/SignUpForm.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: { SignUpForm },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import "./styles/global.sass"
 </style>
