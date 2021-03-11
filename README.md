0c7d0f4a75d24f562afb2ab72f74829a68db429f
lelandshir <lelandshir@gmail.com>
2021-03-11 09:29:04 -0500

    form notes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vue-3-Forms
+# Vue-3-Forms Notes
 
 ### Two-Way Data Binding and the v-model directive
 
@@ -6,3 +6,20 @@
 - The easiest way to track what a user types into an input field is v-model, another vue directive
 - it allows us to sync a form field with component data via two way data binding
 - component data and template inputs are bound
+
+### Useful techniques
+
+- Putting form inputs into an array
+
+### Vue Directive Notes
+
+- When using `v-for`, Vue expects a unique `key` for each element we're looping cycling through
+
+```
+<div v-for="skill in skills" :key="skill">{{skill}}...
+```
+
+### Handle Submit
+
+- Validation
+- Chain the `prevent` event mod onto the `@submit` directive in the form tag === `e.preventDefault()`
--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,16 +1,25 @@
 <template>
-	<form>
+	<!-- FORM -->
+	<form @submit.prevent="handleSubmit">
 		<b>User Credentials:</b>
 		<br />
 		<label>Email:</label>
 		<input type="email" required v-model="email" />
 		<label>Password:</label>
 		<input type="password" required v-model="password" />
+		<div v-if="passwordError" id="error">{{ passwordError }}</div>
 		<label>Role:</label>
 		<select v-model="role">
 			<option value="developer">Web Developer</option>
 			<option value="designer">Web Designer</option>
 		</select>
+		<!-- SKILLS -->
+		<label>Skills</label>
+		<input type="text" v-model="tempSkill" @keyup="addSkill" />
+		<div v-for="skill in skills" :key="skill" class="pill">
+			<!-- using v-for, we have access to the skill / pass into method attd to @click -->
+			<span @click="deleteSkill(skill)">{{ skill }}</span>
+		</div>
 
 		<div class="terms">
 			<label>Accept Terms & Conditions</label>
@@ -33,7 +42,10 @@
 			<label>Envoy Calendar</label>
 			<input type="checkbox" value="Calendar" v-model="options" />
 		</div>
+		<div class="submit"><button>Create Account</button></div>
 	</form>
+	<!-- FORM ENDS -->
+	<!-- Demonstration Area -->
 	<article>
 		<h2>Demonstrating Two-way Data Binding:</h2>
 		<b>Email:</b><br />
@@ -63,7 +75,46 @@ export default {
 			role: "",
 			terms: false,
 			options: [],
+			tempSkill: "",
+			skills: [],
+			passwordError: "",
 		};
+	},
+	methods: {
+		addSkill(e) {
+			console.log(e);
+			if (e.key === "," && this.tempSkill) {
+				if (!this.skills.includes(this.tempSkill)) {
+					this.skills.push(this.tempSkill.replace(",", ""));
+				}
+				this.tempSkill = "";
+			}
+		},
+		deleteSkill(skill) {
+			this.skills = this.skills.filter((item) => {
+				return skill !== item;
+			});
+		},
+		handleSubmit(e) {
+			console.log("Form Submitted");
+			//Validate Password
+			this.passwordError =
+				this.password.length > 5
+					? ""
+					: "password must be at least 6 character long";
+
+			if (!this.passwordError) {
+				const newUser = {
+					email: this.email,
+					password: this.password,
+					role: this.role,
+					skills: this.skills,
+					options: this.options,
+					terms_accepted: this.terms,
+				};
+				console.log(newUser);
+			}
+		},
 	},
 };
 </script>
@@ -116,4 +167,39 @@ article b, form b
 
 article b
     text-decoration: underline
+
+.pill
+    display: inline-block
+    margin: 10px 10px 0 0
+    padding: 6px 12px
+    background: lightblue
+    border-radius: 20px
+    text-align: center
+    font-size: 12px
+    letter-spacing: 1px
+    font-weight: 500
+    color: $primary
+    cursor: pointer
+    &:hover
+      background: $myDarkBlue
+      color: #fff
+
+button
+    background: $primary
+    border: 0
+    border-radius: 20px
+    padding: 10px 20px
+    color: #fff
+    &:hover
+      cursor: pointer
+      background: $myDarkBlue
+      color: #fff
+
+.submit
+    text-align: center
+
+#error
+    color: red
+    margin-top: 10px
+    font-size: 12px
 </style>
